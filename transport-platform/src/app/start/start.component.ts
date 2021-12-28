6045d862cddbd7e1b4d7dc0d7b5e6c48ef7d0595
IrinaGrigore7 <irina.grigore0702@stud.acs.upb.ro>
2021-12-28 21:27:22 +0200

    Users authentication

--- transport-platform/src/app/start/start.component.ts
+++ transport-platform/src/app/start/start.component.ts
@@ -1,40 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import {AngularFireAuth} from '@angular/fire/auth';
-import { FormGroup, FormControl } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.css']
 })
+
 export class StartComponent implements OnInit {
+  login: boolean = false;
+  signup: boolean = false;
 
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
     private fb: FormBuilder,
-    private route: ActivatedRoute,
-    private _snackBar:  MatSnackBar
+    private _snackBar:  MatSnackBar,
+    private fs: AngularFirestore
     ) {}
 
-    loginForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+  loginForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
 
-    signupForm = this.fb.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-      type: ['', Validators.required]
-    });
-    login: boolean = false;
-  signup: boolean = false;
+  signupForm = this.fb.group({
+    username:  ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+    type: ['', Validators.required]
+  });
+  
   public showLogIn() {
     if (this.signup) {
       this.signup = false;
@@ -51,8 +54,21 @@ export class StartComponent implements OnInit {
   LogIn() {
     this.afAuth.signInWithEmailAndPassword(this.loginForm.value.email, this.loginForm.value.password)
     .then(value => {
-      console.log('Nice, it worked!');
-      this.router.navigate(['profile']);
+      this.afAuth.currentUser.then(value => {
+        const uid = value?.uid
+        console.log(uid)
+        const jlogin = this.fs.collection('users').doc(uid)
+        jlogin.ref.get().then((doc) => {
+          const data: ITestItem = doc.data() as ITestItem
+          if (data.type === 'client')
+            this.router.navigate(['client']);
+          if (data.type === 'transporter')
+            this.router.navigate(['transporter']);
+          if (data.type === 'admin')
+            this.router.navigate(['admin']);
+        })
+      
+      })
     })
     .catch(err => {
       this._snackBar.open(err.message, '', {
@@ -65,7 +81,16 @@ export class StartComponent implements OnInit {
   SignUp () {
     this.afAuth.createUserWithEmailAndPassword(this.signupForm.value.email, this.signupForm.value.password)
     .then(value => {
-      if (this.signupForm.value.type === 'transporter') {
+      let item: ITestItem = {
+        email: this.signupForm.value.email,
+        username: this.signupForm.value.username,
+        type: this.signupForm.value.type
+      };
+      this.afAuth.currentUser.then(value => {
+        const uid = value?.uid
+        this.fs.collection('users').doc(uid).set(item);
+      })
+      if (this.signupForm.value.type === 'transporter') {  
         this.router.navigate(['transporter']);
       }
       if (this.signupForm.value.type === 'client') {
@@ -84,5 +109,11 @@ export class StartComponent implements OnInit {
   }
   ngOnInit(): void {
   }
-
 }
+
+export interface ITestItem {
+  email: string,
+  username: string,
+  type: string
+}
+
